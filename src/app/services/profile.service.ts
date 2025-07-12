ce0dc76d3f2d126d289df8c406efd18f19b94b9c
Viacheslav Mikhailov <viacheslav.mikhailov@gmail.com>
2025-07-12 21:29:31 +0300

    Refactor API service imports and add calendar API integration

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { User, BillingPlan, PaymentMethod, Invoice, AppSettings } from '../models/user.model';
 
 @Injectable({providedIn: 'root'})
