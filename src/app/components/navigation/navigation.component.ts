a273567d10963a4808987d0c60ae0e3a31233d0a
Viacheslav Mikhailov <viacheslav.mikhailov@gmail.com>
2025-07-14 20:09:44 +0100

    Fix Authentication System Errors

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
 import { Subject, takeUntil } from 'rxjs';
