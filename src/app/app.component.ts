e353eba046ef92ed155974641e0abd89218616bb
Viacheslav Mikhailov <viacheslav.mikhailov@gmail.com>
2025-07-15 01:10:35 +0100

    Fix mobile scrolling issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   getMainClass(): string {
     return this.activeSection === 'logs' || this.activeSection === 'rules' || this.activeSection === 'calendars'
-      ? 'flex-1 p-5 flex flex-col'
-      : 'flex-1 p-5';
+      ? 'p-5 flex flex-col lg:flex-1'
+      : 'p-5 lg:flex-1';
   }
 }
